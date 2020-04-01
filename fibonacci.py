9fb49f664975335cb51ff2718b88d5aefedc11f9
mahes987 <61834638+mahes987@users.noreply.github.com>
2020-04-01 20:28:17 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+n=int(input("enter the no.of elements:"))
+a=2
+b=3
+c=(a+b)-1
+
+print(a)
+print(b)
+
+
+if (n==1):
+    a=2
+    print(a)
+elif (n==2):
+    b=3
+    print(b)
+for i in range(3,n+1,1):
+    i= (a+b)-1
+        
+    print(i)
+    a=b
+    b=i
+    
+        
+        
